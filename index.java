c7b0fe3c4cb107011525f5ac793a7742aecb4909
Azule2 <69428406+Azule2@users.noreply.github.com>
2022-01-05 12:47:35 +0300

    index.java

---
+++ index.java
@@ -0,0 +1,24 @@
+package rentalStore;
+import java.util.Enumeration;
+import java.util.Vector;
+
+class Cutomeer{
+private String_name;
+private Vector<Rental>_rentals = new Vector<Rental>();
+//IMine
+public Customer(String name) {
+
+_name = name;
+}
+public String getMovie(Movie movie){
+Rental rental = new Rental(nw Movie("",Movie.NEW_RELEASE),10 );
+Movie m =rental._movie;
+return movie.getTitle();
+}
+public void addRental(Rental arg){}
+ -rentals.addElement(arg);
+}
+public String getName(){
+
+return_name;
+}
